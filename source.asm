fd2df1a245044b161ba7946696e3b07785064e67
Fiyinfoluwa Afolayan <fiyinfoluwa.afolayan@gmail.com>
2023-11-27 17:36:44 -0500

    Prompts user for input
    Describes the instructions for asking the user for an integer

--- source.asm
+++ source.asm
@@ -18,3 +18,14 @@ read_int:
     li $v0, 5               # System call for read_int (code 5)
     syscall
     jr $ra                  # Return
+
+# Main program
+main:
+    # Step 1: Prompt user for input
+    la $a0, prompt          # Load address of the prompt
+    jal print_string        # Print the prompt
+
+    # Read integer N from user
+input_loop:
+    jal read_int            # Read user input
+    move $t0, $v0           # Store N in $t0
